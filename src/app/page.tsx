d07ddf76905fa5d606b23deceaedcb7029c4960d
itsmeleonardo <carlosgamercovi20@gmail.com>
2023-06-24 21:03:15 -0500

    change login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,36 +46,60 @@ export default function Login() {
       .finally(() => setLoading(false));
   };
 
-  console.log({ data });
-
   return (
-    <div className="container mx-auto flex items-center justify-center h-screen">
-      <form
-        className="max-w-[400px] w-full flex flex-col gap-4"
-        onSubmit={handleSubmit}
-      >
-        <header className="text-lg font-bold text-center">
-          Iniciar sesión
+    <div className="w-screen h-screen grid grid-cols-3 overflow-hidden">
+      <div className="w-full h-full col-span-2">
+        <picture className="h-full w-full flex items-center relative justify-center">
+          <img
+            className="w-full h-full object-cover absolute top-0 left-0"
+            src="/images/login_img.webp"
+            alt=""
+          />
+        </picture>
+      </div>
+
+      <div className="container mx-auto flex items-center flex-col h-screen px-4 gap-4">
+        <header className="flex flex-col justify-start gap-2 my-12">
+          <header>
+            <h1 className="text-4xl font-bold text-start">
+              Bienvenido a <span className="text-indigo-500">JCU</span> Intranet
+            </h1>
+          </header>
+          <p className="text-sm text-slate-500">
+            Aca podrás encontrar toda la información que necesitas para tu día a
+            día
+          </p>
         </header>
-        <TextInput
-          defaultValue="admin@gmail.com"
-          name="email"
-          placeholder="example@mail.com"
-          type="email"
-        />
-        <TextInput
-          defaultValue="admin123"
-          name="password"
-          placeholder="Contraseña"
-          type="password"
-        />
-        <button
-          type="submit"
-          className="py-2 w-full bg-orange-100 ring-1 ring-orange-400 text-orange-500 text-center rounded-lg"
-        >
-          Login
-        </button>
-      </form>
+        <form className=" w-full flex flex-col gap-4" onSubmit={handleSubmit}>
+          <h3 className="text-lg font-bold text-center">Iniciar sesión</h3>
+          <TextInput
+            defaultValue="admin@gmail.com"
+            name="email"
+            placeholder="example@mail.com"
+            type="email"
+          />
+          <TextInput
+            defaultValue="admin123"
+            name="password"
+            placeholder="Contraseña"
+            type="password"
+          />
+          <button
+            type="submit"
+            className="py-2 w-full bg-indigo-100 ring-1 ring-indigo-400 text-indigo-500 text-center rounded-lg"
+          >
+            Login
+          </button>
+        </form>
+        <footer className="w-full mt-8">
+          <p className="text-sm text-slate-500">
+            Si tienes algún problemas contacta con el{" "}
+            <a href="#" className="text-indigo-500 underline">
+              administrador.
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
