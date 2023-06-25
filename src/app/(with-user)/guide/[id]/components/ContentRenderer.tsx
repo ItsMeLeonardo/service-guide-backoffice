4d155abab26d4f547edc31cb7922f9bf160dcb5f
itsmeleonardo <carlosgamercovi20@gmail.com>
2023-06-25 12:45:35 -0500

    add ia button to guide shower

--- src/app/(with-user)/guide/[id]/components/ContentRenderer.tsx
+++ src/app/(with-user)/guide/[id]/components/ContentRenderer.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import FreeChat from "@/components/AiOptions/FreeChat";
+import AiIcon from "@/icons/AiIcon";
+import { useState } from "react";
 import { YoptaRenderer } from "yopta-editor-copy";
 
 type Props = {
@@ -7,8 +10,26 @@ type Props = {
 };
 
 export default function ContentRenderer({ data }: Props) {
+  const [showChat, setShowChat] = useState(false);
+
   return (
     <div>
+      {showChat && (
+        <FreeChat
+          firstMessage="Puedo ayudarte resolviendo dudas sobre el contenido de esta guia."
+          onClose={() => {
+            setShowChat(false);
+          }}
+        />
+      )}
+      <button
+        className="p-2 bg-white text-indigo-500 rounded-lg absolute right-3 top-3 z-10 hover:bg-indigo-50 cursor-pointer duration-100"
+        onClick={() => {
+          setShowChat(true);
+        }}
+      >
+        <AiIcon />
+      </button>
       <YoptaRenderer data={data} />
     </div>
   );
